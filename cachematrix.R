b341492bb39b731f6aa5eb45e6ee263db33c2ea3
talland <ngoc_tan.nguyen@utt.fr>
2017-08-17 22:42:49 +0200

    Second version of R file

--- cachematrix.R
+++ cachematrix.R
@@ -7,7 +7,7 @@
 ##      that can cache it inverse
 ##      
 makeCacheMatrix <- function(x = matrix()) {
-    invs <- matrix()
+    invs <- NULL
     
     ## return the underlying matrix
     get <- function() x
@@ -18,11 +18,22 @@ makeCacheMatrix <- function(x = matrix()) {
     ## assign value to the matrix itself
     set <- function(y) {
         x <<- y
-        invs <<- matrix() ## new matrix has no cached inverse
+        invs <<- NULL ## new matrix has no cached inverse
     }
     
-    ## set the cached inverse
-    setinverse <- function(inverse) invs <<- inverse
+    ## set the cached inverse. The inverse matrix is checked 
+    ## before being cached actually
+    setinverse <- function(inverse) {
+        # check the inverse matrix before caching
+        checkinverse <- get() %*% inverse
+        midentity <- diag(dim(get())[1])    #identity matrix
+        
+        if (identical(checkinverse, midentity)){
+            invs <<- inverse    
+        }else {
+            message('Not the correct inverse matrix to be cached')
+        }
+    }
     
     list(set = set, get = get,
          setinverse = setinverse,
@@ -41,7 +52,7 @@ cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     invs <- x$getinverse()
     
-    if ( !as.logical(is.na(invs))[1] ) {
+    if ( !is.null(invs) ) {
         # the matrix is unchanged
         message('Retriving the inverse matrix from cache')
         return(invs)
